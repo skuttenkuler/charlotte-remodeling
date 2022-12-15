464adf607d2f8984035109cc6b3dc91e9a9d14ab
skuttenkuler <sam.kuttenk@gmail.com>
2022-12-15 13:54:23 -0600

    inital launch

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -7,11 +7,11 @@ const Footer = () => {
         <img src="https://charlotte-remodeling-assets.s3.us-east-2.amazonaws.com/assets/logo.png"/>
         <h2 className="header">Stay Up To Date</h2>
       <ul className="social">
-      <Link href="#" target="_blank"><img src="https://charlotte-remodeling-assets.s3.us-east-2.amazonaws.com/assets/social-icons/google_icon.png"/></Link>
-      <Link href="#" target="_blank"><img src="https://charlotte-remodeling-assets.s3.us-east-2.amazonaws.com/assets/social-icons/facebook_icon.png"/></Link>
-      <Link href="#" target="_blank"><img src="https://charlotte-remodeling-assets.s3.us-east-2.amazonaws.com/assets/social-icons/instagram_icon.png"/></Link>
-      <Link href="#" target="_blank"><img className="sqr-icon" src="https://charlotte-remodeling-assets.s3.us-east-2.amazonaws.com/assets/social-icons/houzz_icon.png"/></Link>
-      <Link href="#" target="_blank"><img className="sqr-icon" src="https://charlotte-remodeling-assets.s3.us-east-2.amazonaws.com/assets/social-icons/nextdoor_icon.png"/></Link>
+      <a href="#" target="_blank"><img  alt="google icon" src="https://charlotte-remodeling-assets.s3.us-east-2.amazonaws.com/assets/social-icons/google_icon.png"/></a>
+      <a href="#" target="_blank"><img  alt="facebook icon" src="https://charlotte-remodeling-assets.s3.us-east-2.amazonaws.com/assets/social-icons/facebook_icon.png"/></a>
+      <a href="#" target="_blank"><img  alt="instagram icon" src="https://charlotte-remodeling-assets.s3.us-east-2.amazonaws.com/assets/social-icons/instagram_icon.png"/></a>
+      <a href="https://www.houzz.com/professionals/general-contractors/charlotte-remodeling-company-pfvwus-pf~277759379" target="_blank"><img  alt="houzz icon"className="sqr-icon" src="https://charlotte-remodeling-assets.s3.us-east-2.amazonaws.com/assets/social-icons/houzz_icon.png"/></a>
+      <a href="https://nextdoor.com/pages/charlotte-remodeling-company/" target="_blank"><img className="sqr-icon" alt="nextdoor icon" src="https://charlotte-remodeling-assets.s3.us-east-2.amazonaws.com/assets/social-icons/nextdoor_icon.png"/></a>
       </ul>
       <p className="copyright">© Charolotte Remodeling Company</p>
     </footer>
